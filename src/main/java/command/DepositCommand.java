ff6aeacbaa32660d1dfa1e503d83f82baae79b12
novembre manuel <novembremanuel@gmail.com>
2019-07-11 05:59:45 -0700

    MN- fix merge error

--- src/main/java/command/DepositCommand.java
+++
@@ -1,8 +0,0 @@
-package command;
-
-import infra.BankAccountJPA;
-import infra.MoneyJPA;
-
-public interface DepositCommand {
-    BankAccountJPA deposit(String clientId, MoneyJPA moneyToDeposit) throws Exception;
-}
--- src/main/java/command/WithdrawCommand.java
+++
@@ -1,8 +0,0 @@
-package command;
-
-import infra.BankAccountJPA;
-import infra.MoneyJPA;
-
-public interface WithdrawCommand {
-    BankAccountJPA withdraw(String clientId, MoneyJPA moneyToWithdraw) throws Exception;
-}
